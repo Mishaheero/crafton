dd9b9fbfdfc19592b722790e2a632940599a2104
mykhailopo <mykhailopotochnyk@gmail.com>
2024-11-05 09:05:47 +0100

    Refactor: remove unnecessary files and set up project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1 className='lg:text-4xl'>Hello</h1>
+      <header className="App-header">
+        <img src='' className="App-logo" alt="logo" />
+      </header>
+      <h1 className='p-4 text-center text-white bg-blue-500 lg:text-4xl'>Hello</h1>
     </div>
   );
 }
